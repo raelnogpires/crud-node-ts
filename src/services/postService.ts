93bfd301681568db75dc8aaf61db303bafdbb86b
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-12 10:17:22 -0300

    feat: service layer complete

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -6,20 +6,45 @@ export default class PostService {
     private model = new PostModel();
 
     public getAllPosts = async (): Promise<IPost[]> => {
-        const posts = this.model.getAllPosts();
-        if (!posts) {
-            throw new InternalServerError('internal server error');
-        }
+        const posts = await this.model.getAllPosts();
+        if (!posts) throw new InternalServerError('internal server error');
 
         return posts;
     }
 
     public getPostById = async (id: number): Promise<IPost> => {
-        const post = this.model.getPostById(id);
-        if (!post) {
-            throw new NotFoundError('post not found');
-        }
+        const post = await this.model.getPostById(id);
+        if (!post) throw new NotFoundError('post not found');
 
         return post;
+    }
+
+    public createPost = async (p: IPost): Promise<IPost> => {
+        const { title, author, category, publicationDate } = p;
+        const newPost = await this.model.createPost(title, author, category, publicationDate);
+
+        return newPost;
+    }
+
+    public editPost = async (p: IPost): Promise<void> => {
+        const { id, title, author, category, publicationDate } = p;
+        await this.model.editPost(id, title, author, category, publicationDate);
+    }
+
+    public deletePost = async (id: number): Promise<void> => {
+        const exist = await this.model.getPostById(id);
+        if (!exist) throw new NotFoundError('post not found')
+
+        await this.model.deletePost(id);
+    }
+
+    public searchByQuery = async (query: string): Promise<IPost | IPost[]> => {
+        const all = await this.model.getAllPosts();
+        // not the best way to do it
+        const result = all.filter((p) => {
+            p.title.includes(query) || p.author.includes(query) || p.publicationDate.includes(query);
+        });
+
+        return result;
     }
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,5 @@
 import { InternalServerError, NotFoundError } from 'restify-errors';
+import IPost from '../interfaces/post.interface';
 import IUser from "../interfaces/user.interface";
 import UserModel from "../models/user.model";
 
@@ -7,18 +8,14 @@ export default class UserService {
 
     public getAll = async (): Promise<IUser[]> => {
         const users = await this.model.getAllUsers();
-        if (!users) {
-            throw new InternalServerError('internal server error');
-        }
+        if (!users) throw new InternalServerError('internal server error');
 
         return users;
     }
 
     public getUserById = async (id: number): Promise<IUser> => {
         const user = await this.model.getUserById(id);
-        if (!user) {
-            throw new NotFoundError('user not found');
-        }
+        if (!user) throw new NotFoundError('user not found');
 
         return user;
     }
@@ -35,6 +32,9 @@ export default class UserService {
     }
 
     public deleteUser = async (id: number): Promise<void> => {
+        const exist = await this.model.getUserById(id);
+        if (!exist) throw new NotFoundError('user not found')
+
         await this.model.deleteUser(id);
     }
 }
