624c0308cb349a3a99a8e6cd9a6583405280d552
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-12 11:20:12 -0300

    feat: create post.router and userValidation

---
+++ src/middleware/userValidation.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import IUser from "../interfaces/user.interface";
+
+const properties = ['name', 'email', 'password'];
+
+const validateProperties = (post: IUser): [boolean, boolean | string | null] => {
+    for (let i = 0; i < properties.length; i += 1) {
+        if (!Object.prototype.hasOwnProperty.call(post, properties[i])) {
+          return [false, properties[i]];
+        }
+      }
+      return [true, null];
+};
+
+const validateValues = (post: IUser): [boolean, boolean | string | null] => {
+    const entries = Object.entries(post);
+    for (let i = 0; i < entries.length; i += 1) {
+      const [property, value] = entries[i];
+      if (!value) {
+        return [false, property];
+      }
+    }
+    return [true, null];
+};
+
+const userValidation = async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
+    const user = req.body;
+
+    const [valid, property] = validateProperties(user);
+    if (!valid) {
+        return res.status(StatusCodes.BAD_REQUEST).send(`the field ${property} is necessary.`);
+    }
+
+    const [v, p] = validateValues(user);
+    if (!valid) {
+        return res.status(StatusCodes.BAD_REQUEST).send(`the field ${p} can't be null or empty.`);
+    }
+
+    return next();
+};
---
+++ src/routers/post.router.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import PostController from "../controllers/post.controller";
+import postValidation from "../middleware/postValidation";
+
+const router = Router();
+const controller = new PostController();
+
+router
+    .get('/search', controller.searchByQuery)
+    .get('/', controller.getAllPosts)
+    .post('/', postValidation, controller.createPost)
+    .put('/:id', postValidation, controller.editPost)
+    .delete('/:id', controller.deletePost)
+
+export default router;
