4a8c74339656e3a8e10951a24aeed614f3e1d3e0
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-12 15:05:33 -0300

    bug: gitignore wasnt ignoring build

--- build/src/middleware/errorMiddleware.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (err, _req, res, _next) => {
-    console.log(err.message, err);
-    return res.status(err.code).json({ message: err.message });
-};
